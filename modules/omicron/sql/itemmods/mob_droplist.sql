e498cf9b362b1d35794c8e2a538e8466b991412f
Hinekri <106329891+Hinekri@users.noreply.github.com>
2022-05-30 12:19:05 -0500

    Create mob_droplist.sql

---
+++ modules/omicron/sql/itemmods/mob_droplist.sql
@@ -0,0 +1,3 @@
+-- Oupire Drop List Vamp Sword and Garb
+INSERT INTO `xidb`.`mob_droplist` (`dropId`, `itemId`, `itemRate`) VALUES ('1961', '20706', '500');
+INSERT INTO `xidb`.`mob_droplist` (`dropId`, `itemId`, `itemRate`) VALUES ('1961', '2810', '500');
