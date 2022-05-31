97a51a1488356d3b0746e69f458e303a7cdb697a
Hinekri <106329891+Hinekri@users.noreply.github.com>
2022-05-31 15:09:19 -0500

    Update mob_droplist.sql

--- modules/omicron/sql/itemmods/mob_droplist.sql
+++ modules/omicron/sql/itemmods/mob_droplist.sql
@@ -1,3 +1,3 @@
 -- Oupire Drop List Vamp Sword and Garb
 INSERT INTO `mob_droplist` VALUES (1961,0,0,1000,20706,500);
-INSERT INTO `mob_droplist` VALUES (1961,0,0,1000,2810,500);
+INSERT INTO `mob_droplist` VALUES (1961,0,0,1000,26945,500);
