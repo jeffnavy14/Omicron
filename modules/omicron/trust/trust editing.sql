32ad5532e664f2004151f06acf685cd402745b88
Hinekri <106329891+Hinekri@users.noreply.github.com>
2022-06-03 17:35:49 -0500

    balamor

---
+++ modules/omicron/trust/trust editing.sql
@@ -0,0 +1,49 @@
+UPDATE mob_pools SET mJob = 4 WHERE poolid = "5983" AND `name` = 'balamor';
+UPDATE mob_pools SET sJob = 8 WHERE poolid = "5983" AND `name` = 'balamor';
+UPDATE mob_pools SET mJob = 4 WHERE poolid = "5505" AND `name` = 'Balamor';
+UPDATE mob_pools SET sJob = 8 WHERE poolid = "5505" AND `name` = 'Balamor';
+UPDATE `mob_pools` VALUES (5983,'balamor','Balamor',492,0x00001B0C00000000000000000000000000000000,4,8,3,100,100,0,0,0,0,0,0,32,0,3,0,0,396,0,0,1098,492);
+INSERT INTO `mob_skill_lists` VALUES ('TRUST_Balamor',1098,102);      			  -- Guillotine
+INSERT INTO `mob_skill_lists` VALUES ('TRUST_Balamor',1098,935);    			  -- Amon Drive
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,144,13,255);               -- Fire
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,145,38,255);               -- Fire II
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,146,62,255);               -- Fire III
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,147,73,255);               -- Fire IV
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,148,86,255);               -- Fire V
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,149,17,255);               -- Blizzard
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,150,42,255);               -- Blizzard II
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,151,64,255);               -- Blizzard III
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,152,74,255);               -- Blizzard IV
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,153,89,255);               -- Blizzard V
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,154,9,255);                -- Aero
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,155,34,255);               -- Aero II
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,156,59,255);               -- Aero III
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,157,72,255);                -- Aero IV
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,158,83,255);               -- Aero V
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,159,1,255);                -- Stone
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,160,26,255);               -- Stone II
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,161,51,255);               -- Stone III
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,162,68,255);               -- Stone IV
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,163,77,255);               -- Stone V
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,164,21,255);               -- Thunder
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,165,46,255);               -- Thunder II
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,166,66,255);               -- Thunder III
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,167,75,255);               -- Thunder IV
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,168,92,255);               -- Thunder V
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,169,5,255);                -- Water
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,170,30,255);               -- Water II
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,171,55,255);               -- Water III
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,172,70,255);               -- Water IV
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,173,80,255);               -- Water V
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,252,45,255);               -- Stun
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,496,90,255);      	        -- Firaja
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,497,93,255); 	        -- Blizzaja
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,498,87,255);   	        -- Aeroja
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,499,81,255);     	        -- Stoneja
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,500,87,255);     	        -- Thundaja
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,501,84,255);    	        -- Waterja
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,218,99,255); 	   	  -- Meteor
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,255,80,255); 		  -- Break
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,311,80,255); 		  -- Endark
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,367,80,255); 		  -- Death
+INSERT INTO `mob_spell_lists` VALUES ('TRUST_Balamor',396,246,1,255);		 -- Drain II
