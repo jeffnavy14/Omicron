fe68196a95eb1261465c9bf88307ba8c2b600e1a
Hinekri <106329891+Hinekri@users.noreply.github.com>
2022-06-04 17:40:53 -0500

    My Matsu Trust

--- modules/omicron/custom_npc/misc/CatseyeXImod/trusts.lua
+++ modules/omicron/custom_npc/misc/CatseyeXImod/trusts.lua
@@ -515,54 +515,11 @@ m:addOverride("xi.globals.spells.trust.kupofried.onSpellCast", function(caster, 
     trust:SetAutoAttackEnabled(false)
 end)
 
-local trustToReplaceName = "moogle"
-
-m:addOverride(string.format("xi.globals.spells.trust.%s.onSpellCast", trustToReplaceName), function(caster, target, spell)
-
-    -----------------------------------
-    -- NOTE: This is the logic from xi.trust.spawn()
-    -----------------------------------
-    local trust = caster:spawnTrust(spell:getID())
-
-    trust:setModelId(3121) -- Trust: Matsui-P
-    trust:renameEntity("Matsui-P")
-
-    trust:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.COPY_IMAGE, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.UTSUSEMI)
-
-    trust:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, xi.effect.BLINDNESS, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.KURAYAMI)
-    trust:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, xi.effect.SLOW, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.HOJO)
-	
-    trust:addSimpleGambit(ai.t.TARGET, ai.c.MB_AVAILABLE, 0, ai.r.MA, ai.s.MB_ELEMENT, xi.magic.spellFamily.NONE) 
-	
-    trust:addSimpleGambit(ai.t.TARGET, ai.c.NOT_SC_AVAILABLE, 0, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.NONE, 45)
-	
-    trust:addSimpleGambit(ai.t.TARGET, ai.c.READYING_WS, 0, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)
-
-    trust:addSimpleGambit(ai.t.TARGET, ai.c.READYING_MS, 0, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)
-
-    trust:addSimpleGambit(ai.t.TARGET, ai.c.READYING_JA, 0, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)
-
-    trust:addSimpleGambit(ai.t.TARGET, ai.c.CASTING_MA, 0, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.STUN)	
-	
-    trust:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.INNIN, ai.r.JA, ai.s.SPECIFIC, xi.ja.INNIN)
-
-    trust:setTrustTPSkillSettings(ai.tp.OPENER, ai.s.RANDOM)
-	
-	local power = trust:getMainLvl()
-    trust:addMod(xi.mod.MATT, power)
-    trust:addMod(xi.mod.MACC, power)
-	trust:addMod(xi.mod.DEF, power)
-	trust:addMod(xi.mod.MDEF, power)
-	trust:addMod(xi.mod.ATT, power)
-	trust:addMod(xi.mod.ACC, power*4)
-	trust:addMod(xi.mod.DOUBLE_ATTACK, 25)	
-    trust:addMod(xi.mod.MP, 150)	
-end)
 
 m:addOverride(string.format("xi.globals.spells.trust.%s.onMobSpawn", trustToReplaceName), function(mob)
     for _, member in ipairs(mob:getMaster():getParty()) do
         if member:isPC() then
-            member:PrintToPlayer("Matsui-P, Lets get this party started", 4, "Matsui-P") --4: MESSAGE_PARTY
+            member:PrintToPlayer("Kupo?", 4, "Kupofried") --4: MESSAGE_PARTY
         end 
     end
 end)
