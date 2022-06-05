ee9259c06e873e6dca81040855019b643301e32a
jeffnavy14 <jeffnavy14@gmail.com>
2022-06-05 19:03:49 -0400

    final Kirin form

--- modules/omicron/custom_npc/escha_ru/nmspawn.lua
+++ modules/omicron/custom_npc/escha_ru/nmspawn.lua
@@ -165,169 +165,229 @@ m:addOverride("xi.zones.Escha_RuAun.Zone.onZoneTick", function(zone, mob)
                 y = -54.039,
                 z = -597.508,
                 rotation = 50,
-                groupId = 82,
-                groupZoneId = 289,
+                groupId = 64,
+        groupZoneId = 288,
 
-            onMobSpawn = function(mob)
-       	        SetServerVariable("[Eschan]Kirin", 0)
-				SetServerVariable("[Escha]Avatar", 4)
-				mob:setUnkillable(true)
-                mob:hideHP(true)
-            end,	
+        onMobSpawn = function(mob)
+		    SetServerVariable("[Eschan]Kirin", 0)
+            mob:setMod(xi.mod.DEF, 550)
+            mob:setMod(xi.mod.MEVA, 475)
+            mob:setMod(xi.mod.MDEF, 682)
+            -- Make sure model is reset back to start
+            mob:setModelId(403)
+			mob:setMobMod(xi.mobMod.SKILL_LIST, 281)
+            mob:setMobMod(xi.mobMod.SPELL_LIST, 23)
+            -- Prevent death and hide HP until final phase
+            mob:hideHP(true)
+		end,
+        onMobFight = function(mob, target)
+
+	        local lifePercent = mob:getHPP()
+            if lifePercent < 70 then
+			    mob:setModelId(407) -- Genbu
+				mob:setMobMod(xi.mobMod.SKILL_LIST, 277)
+                mob:setMobMod(xi.mobMod.SPELL_LIST, 24)
+				mob:setMod(xi.mod.DEF, 1000)
+                mob:setMod(xi.mod.MEVA, 1000)
+                mob:setMod(xi.mod.STR, 50)
+                mob:setMod(xi.mod.VIT, 20)
+                mob:setMod(xi.mod.INT, 50)
+                mob:setMod(xi.mod.MND, 20)
+                mob:setMod(xi.mod.CHR, 20)
+                mob:setMod(xi.mod.AGI, 20)
+                mob:setMod(xi.mod.DEX, 40)
+                mob:setMod(xi.mod.DEFP, 0)
+                mob:setMod(xi.mod.RATTP, 0)
+                mob:setMod(xi.mod.DEFP, 475)
+                mob:setMod(xi.mod.RATTP, 475)
+                mob:setMod(xi.mod.ACC, 100)
+                mob:setMod(xi.mod.DOUBLE_ATTACK, 5)
+                mob:setMod(xi.mod.EARTH_RES, 170)
+                mob:setMod(xi.mod.DARK_RES, 250)
+                mob:setMod(xi.mod.LIGHT_RES, 128)
+                mob:setMod(xi.mod.FIRE_RES, 170)
+                mob:setMod(xi.mod.WATER_RES, 170)
+                mob:setMod(xi.mod.THUNDER_RES, 170)
+                mob:setMod(xi.mod.ICE_RES, 200)
+                mob:setMod(xi.mod.WIND_RES, 170)
+                mob:setMod(xi.mod.SILENCERES, 100)
+                mob:setMod(xi.mod.FIRE_ABSORB, 100)
+                mob:setMod(xi.mod.STUNRES, 50)
+                mob:setMod(xi.mod.BINDRES, 100)
+                mob:setMod(xi.mod.GRAVITYRES, 100)
+                mob:setMod(xi.mod.SLEEPRES, 100)
+                mob:setMod(xi.mod.PARALYZERES, 100)
+                mob:setMod(xi.mod.LULLABYRES, 100)
+                mob:setMod(xi.mod.FASTCAST, 100)
+                mob:addStatusEffect(xi.effect.REGEN, 50, 3, 0)
+                mob:addStatusEffect(xi.effect.REFRESH, 50, 3, 0)
+	        end
+	        if lifePercent < 60 then
+			    mob:setModelId(399) -- Seiryu
+				mob:setMobMod(xi.mobMod.SKILL_LIST, 278)
+                mob:setMobMod(xi.mobMod.SPELL_LIST, 25)
+			    mob:setMod(xi.mod.DEF, 550)
+                mob:setMod(xi.mod.MEVA, 475)
+                mob:setMod(xi.mod.MDEF, 682)
+                mob:setMod(xi.mod.STR, 40)
+                mob:setMod(xi.mod.VIT, 20)
+                mob:setMod(xi.mod.INT, 50)
+                mob:setMod(xi.mod.MND, 20)
+                mob:setMod(xi.mod.CHR, 20)
+                mob:setMod(xi.mod.AGI, 20)
+                mob:setMod(xi.mod.DEX, 40)
+                mob:setMod(xi.mod.DEFP, 0)
+                mob:setMod(xi.mod.RATTP, 0)
+                mob:setMod(xi.mod.DEFP, 475)
+                mob:setMod(xi.mod.RATTP, 475)
+                mob:setMod(xi.mod.ACC, 150)
+                mob:setMod(xi.mod.EARTH_RES, 128)
+                mob:setMod(xi.mod.DARK_RES, 250)
+                mob:setMod(xi.mod.LIGHT_RES, 200)
+                mob:setMod(xi.mod.ICE_RES, 200)
+                mob:setMod(xi.mod.FIRE_RES, 200)
+                mob:setMod(xi.mod.WATER_RES, 200)
+                mob:setMod(xi.mod.THUNDER_RES, 200)
+                mob:setMod(xi.mod.WIND_RES, 200)
+                mob:setMod(xi.mod.SILENCERES, 100)
+                mob:setMod(xi.mod.STUNRES, 50)
+                mob:setMod(xi.mod.BINDRES, 100)
+                mob:setMod(xi.mod.GRAVITYRES, 100)
+                mob:setMod(xi.mod.SLEEPRES, 100)
+                mob:setMod(xi.mod.POISONRES, 100)
+                mob:setMod(xi.mod.PARALYZERES, 100)
+                mob:setMod(xi.mod.LULLABYRES, 100)
+                mob:setMod(xi.mod.FASTCAST, 10)
+                mob:addStatusEffect(xi.effect.REGEN, 30, 3, 0)
+                mob:addStatusEffect(xi.effect.REFRESH, 50, 3, 0)
+	        end
+	        if lifePercent < 50 then
+			    mob:setModelId(338) -- Suzaku
+				mob:setMobMod(xi.mobMod.SKILL_LIST, 280)
+                mob:setMobMod(xi.mobMod.SPELL_LIST, 27)
+                mob:setMod(xi.mod.STR, 50)
+                mob:setMod(xi.mod.VIT, 20)
+                mob:setMod(xi.mod.INT, 50)
+                mob:setMod(xi.mod.MND, 20)
+                mob:setMod(xi.mod.CHR, 20)
+                mob:setMod(xi.mod.AGI, 50)
+                mob:setMod(xi.mod.DEX, 50)
+                mob:setMod(xi.mod.DEFP, 0)
+                mob:setMod(xi.mod.RATTP, 0)
+                mob:setMod(xi.mod.DEFP, 450)
+                mob:setMod(xi.mod.RATTP, 450)
+                mob:setMod(xi.mod.ACC, 150)
+                mob:setMod(xi.mod.EVA, 100)
+                mob:setMod(xi.mod.TRIPLE_ATTACK, 5)
+                mob:setMod(xi.mod.EARTH_RES, 170)
+                mob:setMod(xi.mod.DARK_RES, 250)
+                mob:setMod(xi.mod.LIGHT_RES, 128)
+                mob:setMod(xi.mod.FIRE_RES, 128)
+                mob:setMod(xi.mod.WATER_RES, 170)
+                mob:setMod(xi.mod.THUNDER_RES, 170)
+                mob:setMod(xi.mod.ICE_RES, 200)
+                mob:setMod(xi.mod.WIND_RES, 170)
+                mob:setMod(xi.mod.SILENCERES, 100)
+                mob:setMod(xi.mod.STUNRES, 50)
+                mob:setMod(xi.mod.BINDRES, 100)
+                mob:setMod(xi.mod.GRAVITYRES, 100)
+                mob:setMod(xi.mod.SLEEPRES, 100)
+                mob:setMod(xi.mod.PARALYZERES, 100)
+                mob:setMod(xi.mod.LULLABYRES, 100)
+                mob:setMod(xi.mod.FASTCAST, 10)
+                mob:addStatusEffect(xi.effect.REGEN, 30, 3, 0)
+                mob:addStatusEffect(xi.effect.REFRESH, 50, 3, 0)
+		    end
+	        if lifePercent < 40 then
+			    mob:setModelId(309) -- Byakko
+				mob:setMobMod(xi.mobMod.SKILL_LIST, 279)
+                mob:setMobMod(xi.mobMod.SPELL_LIST, 26)
+                mob:setMod(xi.mod.STR, 40)
+                mob:setMod(xi.mod.VIT, 20)
+                mob:setMod(xi.mod.INT, 65)
+                mob:setMod(xi.mod.MND, 20)
+                mob:setMod(xi.mod.CHR, 20)
+                mob:setMod(xi.mod.AGI, 20)
+                mob:setMod(xi.mod.DEX, 40)
+                mob:setMod(xi.mod.DEFP, 0)
+                mob:setMod(xi.mod.RATTP, 0)
+                mob:setMod(xi.mod.DEFP, 525)
+                mob:setMod(xi.mod.RATTP, 525)
+                mob:setMod(xi.mod.ACC, 100)
+                mob:setMod(xi.mod.EARTH_RES, 200)
+                mob:setMod(xi.mod.DARK_RES, 250)
+                mob:setMod(xi.mod.LIGHT_RES, 200)
+                mob:setMod(xi.mod.FIRE_RES, 128)
+                mob:setMod(xi.mod.WATER_RES, 200)
+                mob:setMod(xi.mod.THUNDER_RES, 200)
+                mob:setMod(xi.mod.WIND_RES, 200)
+                mob:setMod(xi.mod.ICE_RES, 200)
+                mob:setMod(xi.mod.SILENCERES, 100)
+                mob:setMod(xi.mod.ICE_ABSORB, 100)
+                mob:setMod(xi.mod.STUNRES, 50)
+                mob:setMod(xi.mod.BINDRES, 100)
+                mob:setMod(xi.mod.GRAVITYRES, 100)
+                mob:setMod(xi.mod.SLEEPRES, 100)
+                mob:setMod(xi.mod.POISONRES, 100)
+                mob:setMod(xi.mod.PARALYZERES, 100)
+                mob:setMod(xi.mod.LULLABYRES, 100)
+                mob:setMod(xi.mod.FASTCAST, 10)
+                mob:addStatusEffect(xi.effect.REGEN, 30, 3, 0)
+                mob:addStatusEffect(xi.effect.REFRESH, 50, 3, 0)
+
+		    end
+			if lifePercent < 30 then
+			    mob:setModelId(610) -- Kirin
+				mob:setMobMod(xi.mobMod.SKILL_LIST, 259)
+                mob:setMobMod(xi.mobMod.SPELL_LIST, 109)
+                mob:setMod(xi.mod.STR, 40)
+                mob:setMod(xi.mod.VIT, 20)
+                mob:setMod(xi.mod.INT, 65)
+                mob:setMod(xi.mod.MND, 20)
+                mob:setMod(xi.mod.CHR, 20)
+                mob:setMod(xi.mod.AGI, 20)
+                mob:setMod(xi.mod.DEX, 40)
+                mob:setMod(xi.mod.DEFP, 0)
+                mob:setMod(xi.mod.RATTP, 0)
+                mob:setMod(xi.mod.DEFP, 375)
+                mob:setMod(xi.mod.RATTP, 375)
+                mob:setMod(xi.mod.ACC, 100)
+                mob:setMod(xi.mod.EARTH_RES, 170)
+                mob:setMod(xi.mod.DARK_RES, 240)
+                mob:setMod(xi.mod.LIGHT_RES, 170)
+                mob:setMod(xi.mod.FIRE_RES, 128)
+                mob:setMod(xi.mod.WATER_RES, 170)
+                mob:setMod(xi.mod.THUNDER_RES, 170)
+                mob:setMod(xi.mod.WIND_RES, 128)
+                mob:setMod(xi.mod.ICE_RES, 200)
+                mob:setMod(xi.mod.SILENCERES, 100)
+                mob:setMod(xi.mod.STUNRES, 50)
+                mob:setMod(xi.mod.BINDRES, 100)
+                mob:setMod(xi.mod.GRAVITYRES, 100)
+                mob:setMod(xi.mod.SLEEPRES, 100)
+                mob:setMod(xi.mod.POISONRES, 100)
+                mob:setMod(xi.mod.PARALYZERES, 100)
+                mob:setMod(xi.mod.LULLABYRES, 100)
+                mob:setMod(xi.mod.EARTH_ABSORB, 100)
+                mob:setMod(xi.mod.LIGHT_ABSORB, 100)
+                mob:addStatusEffect(xi.effect.REGEN, 30, 3, 0)
+                mob:setMod(xi.effect.FAST_CAST, 25)
+                mob:addStatusEffect(xi.effect.REFRESH, 50, 3, 0)
+		    end
+	    end,
 			
-    		onMobFight = function(mob, target)
-			    if GetServerVariable("[Escha]Avatar") == 0 then
-				    mob:hideHP(false)
-                    mob:setUnkillable(false)
-				end
-	    	    local lifePercent = mob:getHPP()
-                if lifePercent < 70 and GetServerVariable("Avatar1") == 0 then
-			        SetServerVariable("Avatar1", 1)
-	            end
-	    	    if lifePercent < 45 and GetServerVariable("Avatar2") == 0 then
-			    SetServerVariable("Avatar2", 1)
-	            end
-	    	    if lifePercent < 25 and GetServerVariable("Avatar3") == 0 then
-			        SetServerVariable("Avatar3", 1)
-			    end
-	            if lifePercent < 5 and GetServerVariable("Avatar4") == 0 then
-			        SetServerVariable("Avatar4", 1)
-			    end
-	        end,
-			
-			
-            onMobDeath = function(mob, player, isKiller, noKiller)
-                player:setCharVar("[Eschan]Kirin",1)
-				SetServerVariable("Avatar1", 0)
-				SetServerVariable("Avatar2", 0)
-				SetServerVariable("Avatar3", 0)
-				SetServerVariable("Avatar4", 0)
-            end,
+        onMobDeath = function(mob, playerArg, isKiller)
+
+        end,
 			
 			releaseIdOnDeath = true,
         })
         mob:setDropID(4052)
         mob:setSpawn(-1.52, -54.039, -597.508, 50)
         mob:spawn()
-		mob:setMod(xi.mod.DEF, 450)
-        mob:setMod(xi.mod.MEVA, 380)
-        mob:setMod(xi.mod.MDEF, 50)
-    end
-	
-	              --------------------
-	              -- KIRINS AVATARS --
-	              --------------------
-				  
-				  -- Kirin's Genbu --
-	if GetServerVariable("Avatar1") == 1 then
-        local mob = zone:insertDynamicEntity({
-                objtype = xi.objType.MOB,
-                name = "Kirin's Genbu",
-                x = -11.552,
-                y = -54.040,
-                z = -606.276,
-                rotation = 50,
-                groupId = 15,
-                groupZoneId = 130,
 
-            onMobSpawn = function(mob)
-       	        SetServerVariable("Avatar1", 2)
-            end,	
-    		
-            onMobDeath = function(mob, player, isKiller, noKiller)
-                local Addon = GetServerVariable("[Escha]Avatar")
-                local AddonLeft = math.floor(Addon / 8)
-		            SetServerVariable("[Escha]Avatar", Addon - AddonLeft - 1)
-            end,
-			releaseIdOnDeath = true,
-        })
-        mob:setDropID(0)
-        mob:setSpawn(-11.552, -54.040, -606.276, 50)
-        mob:spawn()
     end
-	
-	               -- Kirin's Suzaku --
-	if GetServerVariable("Avatar2") == 1 then
-        local mob = zone:insertDynamicEntity({
-                objtype = xi.objType.MOB,
-                name = "Kirin's Suzaku",
-                x = 16.562,
-                y = -55.240,
-                z = -605.491,
-                rotation = 50,
-                groupId = 18,
-                groupZoneId = 130,
 
-            onMobSpawn = function(mob)
-                SetServerVariable("Avatar2", 2)
-            end,	
-    		
-            onMobDeath = function(mob, player, isKiller, noKiller)
-                local Addon = GetServerVariable("[Escha]Avatar")
-                local AddonLeft = math.floor(Addon / 8)
-		                SetServerVariable("[Escha]Avatar", Addon - AddonLeft - 1)
-            end,
-			releaseIdOnDeath = true,
-        })
-        mob:setDropID(0)
-        mob:setSpawn(16.562, -55.240, -605.491, 50)
-        mob:spawn()
-    end
-	             -- Kirin's Seiryu --
-	if GetServerVariable("Avatar3") == 1 then
-        local mob = zone:insertDynamicEntity({
-                objtype = xi.objType.MOB,
-                name = "Kirin's Seiryu",
-                x = -11.552,
-                y = -54.040,
-                z = -606.276,
-                rotation = 50,
-                groupId = 16,
-                groupZoneId = 130,
-
-            onMobSpawn = function(mob)
-                SetServerVariable("Avatar3", 2)
-            end,	
-    		
-            onMobDeath = function(mob, player, isKiller, noKiller)
-                local Addon = GetServerVariable("[Escha]Avatar")
-                local AddonLeft = math.floor(Addon / 8)
-		                SetServerVariable("[Escha]Avatar", Addon - AddonLeft - 1)
-            end,
-			releaseIdOnDeath = true,
-        })
-        mob:setDropID(0)
-        mob:setSpawn(-11.552, -54.040, -606.276, 50)
-        mob:spawn()
-    end
-	    
-		        -- Kirin's Byakko --
-	if GetServerVariable("Avatar4") == 1 then
-        local mob = zone:insertDynamicEntity({
-                objtype = xi.objType.MOB,
-                name = "Kirin's Byakko",
-                x = 16.562,
-                y = -55.240,
-                z = -605.491,
-                rotation = 50,
-                groupId = 17,
-                groupZoneId = 130,
-
-            onMobSpawn = function(mob)
-                SetServerVariable("Avatar4", 2)
-            end,	
-    		
-            onMobDeath = function(mob, player, isKiller, noKiller)
-                local Addon = GetServerVariable("[Escha]Avatar")
-                local AddonLeft = math.floor(Addon / 8)
-		            SetServerVariable("[Escha]Avatar", Addon - AddonLeft - 1)
-            end,
-			releaseIdOnDeath = true,
-        })
-        mob:setDropID(0)
-        mob:setSpawn(16.562, -55.240, -605.491, 50)
-        mob:spawn()
-    end
 end)
 
 return m
