0c606dbc01af10906f49109292d0cae9802b65ce
Hinekri <106329891+Hinekri@users.noreply.github.com>
2022-06-09 13:18:39 -0500

    Items

--- modules/omicron/sql/itemmods/item_basic.sql
+++ modules/omicron/sql/itemmods/item_basic.sql
@@ -4,3 +4,4 @@ INSERT INTO `item_basic` VALUES (26115,0,'balder_earring_+1','balder_earring_+1'
 INSERT INTO `item_basic` VALUES (26118,0,'sroda_earring','sroda_earring',1,63572,0,0,0);
 INSERT INTO `item_basic` VALUES (26361,0,'gerdr_belt+1','gerdr_belt+1',1,63572,0,0,0);
 INSERT INTO `item_basic` VALUES (22301,0,'sroda_tathlum','sroda_tathlum',1,63572,0,0,0);
+INSERT INTO `item_basic` VALUES (23797,0,'crepuscular_helm','crepuscular_helm',1,63572,0,1,0);
