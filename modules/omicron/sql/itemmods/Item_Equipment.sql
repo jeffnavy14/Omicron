44d20d85cab30882929624853cd17edb62df3754
Hinekri <106329891+Hinekri@users.noreply.github.com>
2022-06-13 13:40:18 -0500

    Error Clears
    Co-Authored-By: Demetrie <68021436+jeffnavy14@users.noreply.github.com>

--- modules/omicron/sql/itemmods/Item_Equipment.sql
+++ modules/omicron/sql/itemmods/Item_Equipment.sql
@@ -1,3 +1,5 @@
+DELETE FROM `item_equipment` WHERE itemID=26115;
+DELETE FROM `item_equipment` WHERE itemID=22042;
 INSERT INTO `item_equipment` VALUES (26115,'balder_earring_+1',99,119,4194303,0,0,0,6114,0,0);
 INSERT INTO `item_equipment` VALUES (23797,'crepuscular_helm',99,119,10689,103,0,0,16,0,0);
 INSERT INTO `item_equipment` VALUES (26107,'thrud_earring',99,119,10689,0,0,0,6114,0,0);
--- modules/omicron/sql/itemmods/item_weapon.sql
+++ modules/omicron/sql/itemmods/item_weapon.sql
@@ -1,2 +1,3 @@
+DELETE FROM `item_weapon` WHERE itemID=22042;
 INSERT INTO `item_weapon` VALUES (22042,'wizards_rod',11,0,250,250,250,0,1,216,200,0);
  
