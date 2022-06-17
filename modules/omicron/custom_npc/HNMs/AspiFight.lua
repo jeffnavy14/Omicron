be340cecd3231b80d862fd13138550deb94a6139
Hinekri <106329891+Hinekri@users.noreply.github.com>
2022-06-17 08:24:46 -0500

    Aspi

--- modules/omicron/custom_npc/HNMs/AspiFight.lua
+++ modules/omicron/custom_npc/HNMs/AspiFight.lua
@@ -1,56 +1,58 @@
 -----------------------------------
 require("modules/module_utils")
-require("scripts/zones/Behemoths_Dominion/Zone")
+require("scripts/zones/Valley_of_Sorrows/Zone")
 -----------------------------------
-local m = Module:new("KBFight")
+local m = Module:new("AspiFight")
 
 m:addOverride("xi.zones.GM_Home.Zone.onInitialize", function(zone)
     super(zone)
 
-    if GetServerVariable("[EschanKB]") > 1 then
-       SetServerVariable("[EschanKB]", 0)
+    if GetServerVariable("[EschanAspi]") > 1 then
+       SetServerVariable("[EschanAspi]", 0)
 	end
 end)
 
 
-m:addOverride("xi.zones.Behemoths_Dominion.Zone.onZoneTick", function(zone, mob)
+m:addOverride("xi.zones.Valley_of_Sorrows.Zone.onZoneTick", function(zone, mob)
     super(zone)
 
     if
-        GetServerVariable("[EschanKB]") == 1
+        GetServerVariable("[EschanAspi]") == 1 and GetServerVariable("[NinjaTurtle]") == 0
     then
         local mob = zone:insertDynamicEntity({
                 objtype = xi.objType.MOB,
-                name = "Eschan Behemoth",
-                x = -271.3363,
-                y = -19.3878,
-                z = 61.8363,
+                name = "Ninja Turtle",
+                x = -5.4115,
+                y = 0.1944,
+                z = -33.4813,
                 rotation = 50,
-                groupId = 10,
-                groupZoneId = 127,
+                groupId = 7,
+                groupZoneId = 128,
 
         onMobSpawn = function(mob)
-		    SetServerVariable("[EschanKB]", 0)
+		    SetServerVariable("[EschanAspi]", 0)
+		     player:setCharVar("[NinjaTurtle]", 1)
+		
             end,	
     		
             onMobDeath = function(mob, player, isKiller, noKiller)
-
+		     player:setCharVar("[NinjaTurtle]", 0)
             end,
 			releaseIdOnDeath = true,
         })
 
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 479)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 126)
             mob:hideHP(true)
 
-        mob:setDropID(4055)
-        mob:setSpawn(-271.3363, -19.3878, 61.8363, 50)
+        mob:setDropID(4056)
+        mob:setSpawn(-5.4115, 0.1944, -33.4813, 50)
         mob:spawn()
+		mob:setMobMod(xi.mobMod.SKILL_LIST, 479)
+            mob:setMobMod(xi.mobMod.SPELL_LIST, 126)
 		    mob:setMobLevel(125)
-		    mob:setMod(xi.mod.DEF, 1100)
-                mob:setMod(xi.mod.MDEF, 1400)
-                mob:setMod(xi.mod.MEVA, 500)
-                mob:setMod(xi.mod.EVA, 700)
+		    mob:setMod(xi.mod.DEF, 2000)
+                mob:setMod(xi.mod.MDEF, 1100)
+                mob:setMod(xi.mod.MEVA, 900)
+                mob:setMod(xi.mod.EVA, 900)
                 mob:setMod(xi.mod.STR, 200)
                 mob:setMod(xi.mod.VIT, 300)
                 mob:setMod(xi.mod.INT, 150)
@@ -58,18 +60,18 @@ m:addOverride("xi.zones.Behemoths_Dominion.Zone.onZoneTick", function(zone, mob)
                 mob:setMod(xi.mod.CHR, 120)
                 mob:setMod(xi.mod.AGI, 120)
                 mob:setMod(xi.mod.DEX, 140)
-                mob:setMod(xi.mod.MATT,100)
+                mob:setMod(xi.mod.MATT,150)
                 mob:setMod(xi.mod.ACC, 1000)
-                mob:setMod(xi.mod.DOUBLE_ATTACK, 15)
-                mob:setMod(xi.mod.EARTH_RES, 200)
-                mob:setMod(xi.mod.DARK_RES, 250)
-                mob:setMod(xi.mod.LIGHT_RES, 150)
-                mob:setMod(xi.mod.FIRE_RES, 200)
-                mob:setMod(xi.mod.WATER_RES, 200)
-                mob:setMod(xi.mod.THUNDER_RES, 300)
+                mob:setMod(xi.mod.DOUBLE_ATTACK, 25)
+                mob:setMod(xi.mod.EARTH_RES, 400)
+                mob:setMod(xi.mod.DARK_RES, 450)
+                mob:setMod(xi.mod.LIGHT_RES, 450)
+                mob:setMod(xi.mod.FIRE_RES, 400)
+                mob:setMod(xi.mod.WATER_RES, 400)
+                mob:setMod(xi.mod.THUNDER_RES, 500)
                 mob:setMod(xi.mod.ICE_RES, 0)
-                mob:setMod(xi.mod.WIND_RES, 200)
-                mob:setMod(xi.mod.SILENCERES, 500)
+                mob:setMod(xi.mod.WIND_RES, 400)
+                mob:setMod(xi.mod.SILENCERES, 600)
                 mob:setMod(xi.mod.FIRE_ABSORB, 100)
                 mob:setMod(xi.mod.BINDRES, 1000)
                 mob:setMod(xi.mod.GRAVITYRES, 200)
