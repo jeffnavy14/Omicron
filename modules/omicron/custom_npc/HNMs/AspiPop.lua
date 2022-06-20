728587269a6c103800ea3f1eb4e556f126e7bbc2
Demetrie <68021436+jeffnavy14@users.noreply.github.com>
2022-06-19 21:54:12 -0400

    fixed

--- modules/omicron/custom_npc/HNMs/AspiPop.lua
+++ modules/omicron/custom_npc/HNMs/AspiPop.lua
@@ -1,9 +1,9 @@
------------------------------------
+------------------------------------
 require("modules/module_utils")
 require("scripts/zones/Valley_of_Sorrows/Zone")
+require("scripts/globals/keyitems")
 -----------------------------------
 local m = Module:new("AspiPop")
-m:setEnabled(true)
 
 m:addOverride("xi.zones.Valley_of_Sorrows.Zone.onInitialize", function(zone)
        super(zone)
@@ -19,15 +19,17 @@ local aspi = zone:insertDynamicEntity({
         rotation = 180,
 
         onTrigger = function(player, npc)
-        if player:getGMLevel() > 0 and player:checkNameFlags(0x04000000) then
-		   SetServerVariable("[EschanAspi]", 1)
-        end
-        if GetServerVariable("[EschanAspi]") == 2 then
-            player:PrintToPlayer("Mob is still active, please wait!")
-        elseif GetServerVariable("[EschanAspi]") == 0 then
-            SetServerVariable("[EschanAspi]", 1) 
-        end            
-	end,
+            if player:getGMLevel() > 0 and player:checkNameFlags(0x04000000) then
+		       SetServerVariable("[EschanAspi]", 1)
+            end
+        end,			
+        onTrade = function(player, npc, trade)	
+		    if GetServerVariable("[EschanAspi]") == 0 and player:hasKeyItem(xi.ki.KUPOFRIEDS_MEDALLION) and (trade:hasItemQty(3344,1)) then -- Pond Weed
+                player:tradeComplete()
+				player:delKeyItem(xi.ki.KUPOFRIEDS_MEDALLION)
+                SetServerVariable("[EschanAspi]", 1) 
+			end
+		end,
 	})
 end)
 
