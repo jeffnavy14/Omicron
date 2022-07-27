4456a89a9177a5091f807369dbbf5271391f3c19
Hinekri <106329891+Hinekri@users.noreply.github.com>
2022-07-27 10:20:58 -0500

    Update Muffin.lua
    Co-Authored-By: Xaver-DaRed <60053999+Xaver-DaRed@users.noreply.github.com>

--- modules/omicron/NMs/Muffin.lua
+++ modules/omicron/NMs/Muffin.lua
@@ -7,55 +7,38 @@ local m = Module:new("Double1")
 m:addOverride("xi.zones.GM_Home.Zone.onInitialize", function(zone)
     super(zone)
 
-    if GetServerVariable("[Double1]") > 1 then
+    if GetServerVariable("[Double1]") > 0 then
        SetServerVariable("[Double1]", 0)
 	end
 end)
 
 
-m:addOverride("xi.zones.Attohwa_Chasm.Zone.onZoneTick", function(zone, mob)
+m:addOverride("xi.zones.Attohwa_Chasm.Zone.inZoneTick", function(zone, mob)
     super(zone)
 
-    if
-        GetServerVariable("[Double1]") == 0
-    then
+    if GetServerVariable("[Double1]") == 0 then
         local mob = zone:insertDynamicEntity({
-                objtype = xi.objType.MOB,
-                name = "Muffin",
-		    look = "0x00000E0100000000000000000000000000000000",
-                x = 307.9126,
-                y = -24.8447,
-                z = 15.6445,
-                rotation = 50,
-                groupId = 10,
-                groupZoneId = 146,
+            objtype     = xi.objType.MOB,
+            name        = "Muffin",
+            look        = "0x00000E0100000000000000000000000000000000",
+            x           = 307.9126,
+            y           = -24.8447,
+            z           = 15.6445,
+            rotation    = 50,
+            groupId     = 10,
+            groupZoneId = 146,
 
-        onMobSpawn = function(mob)
-		    SetServerVariable("[Double1]", 1)
-		    SetServerVariable("[Set1]", math.random(1,20))
-            end,	
-    		
-            onMobDeath = function(mob, player, isKiller, noKiller)
-		    SetServerVariable("[Double1]", 0)
-            end,
-			releaseIdOnDeath = true,
+            onMobSpawn = function(mob)
+                SetServerVariable("[Double1]", 1)
+                SetServerVariable("[Set1]", math.random(1,20))
 
-        mixins =
-        {
-            require("scripts/mixins/rage"),
-            require("scripts/mixins/job_special"),
-        },
-
-        -- The "whooshy" special animation that plays when Trusts or Dynamis mobs spawn
-        specialSpawnAnimation = true,
-        })
-
-            mob:hideHP(true)
-        	mob:setDropID(4105)
-        	mob:spawn()
-		mob:setMobMod(xi.mobMod.CHECK_AS_NM, 1)
-		    mob:setMobLevel(125)
-		    mob:setMod(xi.mod.DEF, 1200)
+                -- Modifiers
+                mob:hideHP(true)
+                mob:setDropID(4105)
+                mob:spawn()
+                mob:setMobMod(xi.mobMod.CHECK_AS_NM, 1)
+                mob:setMobLevel(125)
+                mob:setMod(xi.mod.DEF, 1200)
                 mob:setMod(xi.mod.MDEF, 300)
                 mob:setMod(xi.mod.MEVA, 400)
                 mob:setMod(xi.mod.EVA, 750)
@@ -90,110 +73,88 @@ m:addOverride("xi.zones.Attohwa_Chasm.Zone.onZoneTick", function(zone, mob)
                 mob:setMod(xi.mod.ALL_WSDMG_ALL_HITS, 10)
                 mob:addStatusEffect(xi.effect.REGEN, 20, 3, 0)
                 mob:addStatusEffect(xi.effect.REFRESH, 50, 3, 0)
-	      end
-	if
-		GetServerVariable("[Set1]") == 1 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 1027)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 1)
 
-	elseif
-		GetServerVariable("[Set1]") == 2 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 1024)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 2)
+                -- Skill sets.
+                if GetServerVariable("[Set1]") == 1 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 1027)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 1)
+                elseif GetServerVariable("[Set1]") == 2 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 1024)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 2)
+                elseif GetServerVariable("[Set1]") == 3 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 1025)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 3)
+                elseif GetServerVariable("[Set1]") == 4 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 1018)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 4)
+                elseif GetServerVariable("[Set1]") == 5 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 1075)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 5)
+                elseif GetServerVariable("[Set1]") == 6 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 1016)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 6)
+                elseif GetServerVariable("[Set1]") == 7 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 1015)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 7)
+                elseif GetServerVariable("[Set1]") == 8 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 1018)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 8)
+                elseif GetServerVariable("[Set1]") == 9 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 854)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 12)
+                elseif GetServerVariable("[Set1]") == 10 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 861)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 13)
+                elseif GetServerVariable("[Set1]") == 11 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 864)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 14)
+                elseif GetServerVariable("[Set1]") == 12 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 869)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 15)
+                elseif GetServerVariable("[Set1]") == 13 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 874)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 16)
+                elseif GetServerVariable("[Set1]") == 14 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 877)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 17)
+                elseif GetServerVariable("[Set1]") == 15 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 882)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 18)
+                elseif GetServerVariable("[Set1]") == 16 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 892)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 19)
+                elseif GetServerVariable("[Set1]") == 17 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 895)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 21)
+                elseif GetServerVariable("[Set1]") == 18 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 898)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 22)
+                elseif GetServerVariable("[Set1]") == 19 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 899)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 28)
+                elseif GetServerVariable("[Set1]") == 20 then
+                    mob:setMobMod(xi.mobMod.SKILL_LIST, 903)
+                    mob:setMobMod(xi.mobMod.SPELL_LIST, 29)
+                end
+            end,
 
-	elseif
-		GetServerVariable("[Set1]") == 3 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 1025)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 3)
+            onMobDeath = function(mob, player, isKiller, noKiller)
+                SetServerVariable("[Double1]", 0)
+            end,
 
-	elseif
-		GetServerVariable("[Set1]") == 4 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 1018)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 4)
+            releaseIdOnDeath = true,
 
-	elseif
-		GetServerVariable("[Set1]") == 5 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 1075)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 5)
+            mixins =
+            {
+                require("scripts/mixins/rage"),
+                require("scripts/mixins/job_special"),
+            },
 
-	elseif
-		GetServerVariable("[Set1]") == 6 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 1016)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 6)
+            -- The "whooshy" special animation that plays when Trusts or Dynamis mobs spawn
+            specialSpawnAnimation = true,
+        })
+    end
 
-	elseif
-		GetServerVariable("[Set1]") == 7 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 1015)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 7)
-
-	elseif
-		GetServerVariable("[Set1]") == 8 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 1018)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 8)
-
-	elseif
-		GetServerVariable("[Set1]") == 9 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 854)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 12)
-
-	elseif
-		GetServerVariable("[Set1]") == 10 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 861)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 13)
-
-	elseif
-		GetServerVariable("[Set1]") == 11 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 864)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 14)
-
-	elseif
-		GetServerVariable("[Set1]") == 12 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 869)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 15)
-
-	elseif
-		GetServerVariable("[Set1]") == 13 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 874)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 16)
-
-	elseif
-		GetServerVariable("[Set1]") == 14 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 877)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 17)
-
-	elseif
-		GetServerVariable("[Set1]") == 15 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 882)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 18)
-
-	elseif
-		GetServerVariable("[Set1]") == 16 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 892)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 19)
-
-	elseif
-		GetServerVariable("[Set1]") == 17 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 895)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 21)
-
-	elseif
-		GetServerVariable("[Set1]") == 18 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 898)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 22)
-
-	elseif
-		GetServerVariable("[Set1]") == 19 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 899)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 28)
-
-	elseif
-		GetServerVariable("[Set1]") == 20 then
-		mob:setMobMod(xi.mobMod.SKILL_LIST, 903)
-            mob:setMobMod(xi.mobMod.SPELL_LIST, 29)
-	end
-
-		
 end)
 
 return m
-		
