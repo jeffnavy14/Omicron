434c1f6ee69df2ea785b7c1845edd72f90a6b44b
Demetrie <68021436+jeffnavy14@users.noreply.github.com>
2023-04-24 20:41:58 -0400

    Create Jamal.lua

---
+++ scripts/zones/RuLude_Gardens/npcs/Jamal.lua
@@ -0,0 +1,42 @@
+-----------------------------------
+-- Cipher: Rainemard Quest
+-- Area: Ru'Lude Gardens
+--  NPC: Jamal
+-- !pos 3.779 2.000 140.932 243
+-----------------------------------
+require('scripts/globals/keyitems')
+require('scripts/globals/items')
+require('scripts/globals/npc_util')
+require('scripts/globals/settings')
+-----------------------------------
+
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+    local StartingTrusts = player:hasSpell(896) and player:hasSpell(897) and player:hasSpell(898) and player:hasSpell(899) and
+        player:hasSpell(900) and player:hasSpell(901) and player:hasSpell(902) and player:hasSpell(903) and
+        player:hasSpell(904) and player:hasSpell(905) and player:hasSpell(917)
+
+    if StartingTrusts then
+	    npcUtil.giveKeyItem(player, xi.keyItem.BUNDLE_OF_HALF_INSCRIBED_SCROLLS)
+        player:PrintToPlayer("Impressive, adventurer. I never doubted you for a moment. Here, take this as a token of recognition for your effects.", 0, npc:getPacketName())
+    --else
+    --    player:PrintToPlayer("Hey, you... Looking for some exotic Trust Magic?", 0, npc:getPacketName())
+	
+    --    player:timer(2000, function(playerArg)	
+    --    player:PrintToPlayer("Collect red, blue and yellow institute cards, and I'll reward you with something special.", 0, npc:getPacketName())	
+    --    end)
+    --    return
+	end	
+end
+
+entity.onEventUpdate = function(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option)
+end
+
+return entity
