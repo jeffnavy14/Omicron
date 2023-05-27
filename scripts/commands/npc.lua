c7e8709f37f0ca8bef01a2062907d1018cfbe8b0
Demetrie <68021436+jeffnavy14@users.noreply.github.com>
2023-05-27 11:57:53 -0400

    Update npc.lua

--- scripts/commands/npc.lua
+++ scripts/commands/npc.lua
@@ -5,6 +5,7 @@
 -----------------------------------
 require('scripts/globals/keyitems')
 require('scripts/globals/npc_util')
+require('scripts/globals/status')
 -----------------------------------
 
 cmdprops =
@@ -17,7 +18,7 @@ function onTrigger(player)
     local zone = player:getZone()
     local npc = zone:insertDynamicEntity({
     objtype = xi.objType.NPC,
-    name = "PollyB",
+    name = "?????",
 	look = 3613,
     x = player:getXPos(),
     y = player:getYPos(),
@@ -26,11 +27,27 @@ function onTrigger(player)
 
 	
 	onTrigger = function(player, npc, status)
-    local race = player:getRace()
-	if race == 8 then
-	player:PrintToPlayer(string.format("damn hume (%s)", npc:getPacketName()))
-    end
+    player:PrintToPlayer("???? : I can make you a Iridal Staff.  Bring me all 8 NQ Staves and I will make you what you need", 0xD);
+    player:PrintToPlayer("???? : I can also make you a Chatoyant Staff.  Bring me all 8 HQ staves and I will see what I can do", 0xD);
     end,   
+	onTrade = function(player, npc)
+
+	  ---------------------------------
+      --    Ele Staff                --
+      ---------------------------------
+    if (trade:hasItemQty(17545,1)) and (trade:hasItemQty(17547,1)) and (trade:hasItemQty(17549,1)) and (trade:hasItemQty(17551,1))
+          and (trade:hasItemQty(17553,1)) and (trade:hasItemQty(17555,1)) and (trade:hasItemQty(17557,1)) and (trade:hasItemQty(17559,1)) then
+          player:tradeComplete()
+          player:messageSpecial(ID.text.ITEM_OBTAINED, 18632);
+          player:addItem(18632)
+    end
+    if trade:hasItemQty(17546,1) and trade:hasItemQty(17548,1) and trade:hasItemQty(17550,1) and trade:hasItemQty(17552,1)
+          and trade:hasItemQty(17554,1) and trade:hasItemQty(17556,1) and trade:hasItemQty(17558,1) and trade:hasItemQty(17560,1) then
+          player:tradeComplete()
+          player:messageSpecial(ID.text.ITEM_OBTAINED, 18633);
+          player:addItem(18633)
+    end	  
+	  end,
     })
     player:PrintToPlayer(string.format("Please move to spawn (%s)", npc:getPacketName()))
 end
