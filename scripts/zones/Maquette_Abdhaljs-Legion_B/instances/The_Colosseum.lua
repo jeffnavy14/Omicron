f811373a360615ae1a7fe3abee1589c9aa66eeda
aliron <drunk_cunuk@hotmail.com>
2025-07-13 15:42:41 -0400

    Update The_Colosseum.lua

--- scripts/zones/Maquette_Abdhaljs-Legion_B/instances/The_Colosseum.lua
+++ scripts/zones/Maquette_Abdhaljs-Legion_B/instances/The_Colosseum.lua
@@ -8,37 +8,38 @@ require("modules/omicronXI/global/colosseum")
 local instanceObject = {}
 
 instanceObject.onInstanceCreated = function(instance, player)
-omicron.colosseum_util.onInstanceCreated(instance, player)
+    omicron.colosseum_util.onInstanceCreated(instance, player)
 end
 
 instanceObject.onInstanceCreatedCallback = function(player, instance)
-omicron.colosseum_util.onInstanceCreatedCallback(player, instance)
+    omicron.colosseum_util.onInstanceCreatedCallback(player, instance)
 end
 
 instanceObject.afterInstanceRegister = function(player)
-omicron.colosseum_util.onInstanceCreatedCallback(player, instance)
+    omicron.colosseum_util.afterInstanceRegister(player, instance)
 end
 
 instanceObject.onInstanceTimeUpdate = function(instance, elapsed)
-omicron.colosseum_util.onInstanceTimeUpdate(instance, elapsed)
+    omicron.colosseum_util.onInstanceTimeUpdate(instance, elapsed)
 end
 
 instanceObject.onInstanceFailure = function(instance)
-    xi.ambuscade.onInstanceFailure(instance)
+    omicron.colosseum_util.onInstanceFailure(instance)
 end
 
 instanceObject.onInstanceProgressUpdate = function(instance, progress)
 end
 
 instanceObject.onInstanceComplete = function(instance)
-omicron.colosseum_util.onInstanceComplete(instance)
+    omicron.colosseum_util.onInstanceComplete(instance)
 end
 
 instanceObject.onEventUpdate = function(player, csid, option, npc)
+    omicron.colosseum_util.onEventUpdate(player, csid, option, npc)
 end
 
 instanceObject.onEventFinish = function(player, csid, option, npc)
-omicron.colosseum_util.onEventFinish(player, csid, option, npc)
+    omicron.colosseum_util.onEventFinish(player, csid, option, npc)
 end
 
 return instanceObject
