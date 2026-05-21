5748ebf0f75656b9a6b9a7ff8c38fd910c28fd60
Demetrie <68021436+jeffnavy14@users.noreply.github.com>
2026-05-21 19:58:47 -0400

    zone_awake patch

--- src/map/zone.cpp
+++ src/map/zone.cpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
 ===========================================================================
 
   Copyright (c) 2010-2015 Darkstar Dev Teams
@@ -230,6 +230,17 @@ void CZone::SetBackgroundMusicDay(uint16 music)
 void CZone::SetBackgroundMusicNight(uint16 music)
 {
     m_zoneMusic.m_songNight = music;
+}
+
+void CZone::SetPreventSleep(bool value)
+{
+    m_preventSleep = value;
+
+    if (m_preventSleep && !zoneTimerToken_.has_value())
+    {
+        createZoneTimers();
+        ShowInfoFmt("Zone {} ({}) forced awake by SetPreventSleep.", GetID(), getName());
+    }
 }
 
 /**
@@ -954,7 +965,7 @@ auto CZone::ZoneServer(timer::time_point tick) -> Task<void>
         m_BattlefieldHandler->HandleBattlefields(tick);
     }
 
-    if (zoneTimerToken_.has_value() && m_zoneEntities->CharListEmpty() && m_timeZoneEmpty + 5s < timer::now() && CheckMobsPathedBack())
+    if (!m_preventSleep && zoneTimerToken_.has_value() && m_zoneEntities->CharListEmpty() && m_timeZoneEmpty + 5s < timer::now() && CheckMobsPathedBack())
     {
         zoneTimerToken_.reset();
         zoneTimerTriggerAreasToken_.reset();
